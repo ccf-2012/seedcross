4b51111f7d8623272311224a26a696f2f1157fc8
ccf2012 <crlf66@163.com>
2022-01-22 02:00:17 +0800

    db

--- csui/settings.py
+++ csui/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'csui.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db/db.sqlite3',
     }
 }
 
