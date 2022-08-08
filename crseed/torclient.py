e2866a121fb7622e5f3a6bd8da985aaff7bd7e91
ccf2012 <crlf66@163.com>
2022-08-08 17:16:21 +0800

    protect tor.trackers[3]

--- crseed/torclient.py
+++ crseed/torclient.py
@@ -182,7 +182,8 @@ class QbDownloadClient(DownloadClientBase):
         return abbrev
 
     def mkSeedTor(self, tor):
-        trackerstr = tor.tracker if len(tor.tracker)>1 else tor.trackers[3]["url"]
+        ltr = 3 if len(tor.trackers) >= 3 else len(tor.trackers)
+        trackerstr = tor.tracker if len(tor.tracker)>1 else tor.trackers[ltr]["url"]
         st = SeedingTorrent(
             torrent_hash=tor.hash,
             name=tor.name,
